0955fde6fb748453c7fa7676483880bd0bfcbacb
koyamal <74910401+koyamal@users.noreply.github.com>
2022-09-20 21:02:09 +0900

    Add ChildArea.jsx

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import { useState } from "react";
+import { ChildArea } from "./components/ChildArea";
 import "./styles.css";
 
 export default function App() {
-  const [count, setCount] = useState(0);
-  const onClickCountUp = () => {
-    setCount(count + 1);
+  // const [count, setCount] = useState(0);
+  // const onClickCountUp = () => {
+  //   setCount(count + 1);
+  // };
+  const [text, setText] = useState("");
+  const [open, setOpen] = useState(false);
+  const OnChangeText = (e) => {
+    setText(e.target.value);
+  };
+  const onClickOpen = () => {
+    setOpen(!open);
   };
 
   return (
     <div className="App">
-      <p>{count}</p>
-      <button onClick={onClickCountUp}>CountUp</button>
+      <input value={text} onChange={OnChangeText} />
+      <br />
+      <br />
+      <button onClick={onClickOpen}>表示</button>
+      <ChildArea open={open} />
+      {/* <p>{count}</p>
+      <button onClick={onClickCountUp}>CountUp</button> */}
     </div>
   );
 }
